60e2581b51324e1e1ff164bc8e494f60b4da54e1
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-18 23:39:34 +0530

    Refactor User registration and login logic
    Refactored user registration and login methods to include email existence check and password validation.

--- Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/service/impl/UserServiceImpl.java
+++ Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/service/impl/UserServiceImpl.java
@@ -9,12 +9,9 @@ import org.springframework.stereotype.Service;
 =========================================================
 WHAT IS THIS CLASS?
 =========================================================
-
 This class contains BUSINESS LOGIC for users.
-
  Controller calls this
  This class talks to repository
-
 =========================================================
 */
 
@@ -29,30 +26,39 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(User user) {
-
-        // =========================
-        // TASK
-        // =========================
         // STEP 1: check if email already exists
+        User existing = userRepo.findByEmail(user.getEmail());
+
         // STEP 2: if exists → stop process
+        if (existing != null) {
+            return null;
+        }
         // STEP 3: if not → save user to DB
         // STEP 4: return saved user
-
-        return null;
+        else {
+            return userRepo.save(user);
+        }
     }
 
     @Override
     public User login(String email, String password) {
-
-        // =========================
-        // TASK
-        // =========================
         // STEP 1: find user by email
+        User user = userRepo.findByEmail(email);
+
         // STEP 2: if user not found → return null
+        if (user == null) {
+            return null;
+        }
         // STEP 3: check password match
         // STEP 4: if correct → return user
         // STEP 5: else → return null
-
-        return null;
+        else {
+            if (user.getPassword().equals(password)) {
+                return user;
+            }
+            else {
+                return null;
+            }
+        }
     }
 }
